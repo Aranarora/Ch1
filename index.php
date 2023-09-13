2c6ad2d119d460795f9fca9f4f53f894b207cbc8
Aranarora <aranarora007@gmail.com>
2023-09-12 17:09:22 -0700

    Add the comments to separate each try this operation

--- index.php
+++ index.php
@@ -37,9 +37,11 @@ $best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Licorice',
   </head>
   <body>
     <h1>The Candy Store</h1>
+    <!-- echo-shorthaand -->
     <h2>Welcome <?php echo $name; ?></h2>
     <p>Your favorite type of candy is:
        <?= $favorites[0] ?>.</p>
+    <!-- Indexd array -->
     <h2>Best Sellers</h2>
     <ul>
       <li><?php echo $best_sellers[0]; ?></li>
@@ -48,12 +50,15 @@ $best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Licorice',
       <li><?php echo $best_sellers[4]; ?></li>
       <li><?php echo $best_sellers[5]; ?></li>
     </ul>
+    <!-- Updating array -->
+    <!-- Associative array -->
     <h2>Nutrition (per 100g)</h2>
     <p>Fat:   <?php echo $nutrition['fat']; ?>%</p>
     <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
     <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
     <p>Protein:  <?php echo $nutrition['protein']; ?>%</p>
     <p>Fiber:  <?php echo $nutrition['fiber']; ?>%</p>
+    <!-- Multidimensional array -->
     <h2>Offers</h2>
     <p><?php echo $offers[0]['name']; ?> -
       $<?php echo $offers[0]['price']; ?> </p>
@@ -65,12 +70,14 @@ $best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Licorice',
       $<?php echo $offers[3]['price']; ?> </p>
     <p>The cost of your candy is 
        $<?php echo $price; ?> per pack.</p>
+    <!-- Arithmetic Operation -->
     <h2>Shopping Cart</h2>
     <p>Items: <?= $items ?></p>
     <p>Cost per pack: $<?= $cost ?></p>
     <p>Subtotal: $<?= $subtotal ?></p>
     <p>Tax: $<?= $tax ?></p>
     <p>Total: $<?= $total ?></p>
+    <!-- String operation -->
     <h2><?= $name ?>'s Order</h2>
     <p><?= $message ?></p>
   </body>
